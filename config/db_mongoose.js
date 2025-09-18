4982ff6da899f68ee95e57114f051881a9e058b9
Leonardo Silva e Cruz <101243664+HiperD@users.noreply.github.com>
2025-09-18 11:32:57 -0300

    Update db_mongoose.js

--- config/db_mongoose.js
+++ config/db_mongoose.js
@@ -1,4 +1,5 @@
 const StringCon = {
-    connection: "mongodb+srv://hiiperdelicia:58028814Leonardo10%40@cluster0.dsybkq6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/WebBack"
+    connection: "mongodb+srv://"
 };
+
 module.exports = StringCon;
